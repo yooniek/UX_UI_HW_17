380bd4b3a6dadd021a4f43b840e733b31a187552
yooniek <heather.yh.k@gmail.com>
2022-02-14 21:10:03 -0500

    feb 14

---
+++ UX_UI_HW_19/Index.js
@@ -0,0 +1,12 @@
+console.log("Your Index.js file is loaded correctly");
+
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
+
+$(document).ready(function(){
+    $("button").mouseenter(function(){
+      alert("YOONHEE KIM'S RESUME IS NOT AVAILABLE AT THE MOMENTS.");
+    });
+  });
